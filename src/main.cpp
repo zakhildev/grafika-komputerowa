f5f6e1bdaf6ccaf63a891981f12a91555eaf0e86
zakhildev <piotr.blecharski02@gmail.com>
2026-04-19 17:42:49 +0200

    refactor: Move camera to its own class

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-﻿#include "include/Mesh.hpp"
-#include <string>
-#define GLM_FORCE_RADIANS
+﻿#define GLM_FORCE_RADIANS
 #define STB_IMAGE_IMPLEMENTATION
 
-#include "./include/Callbacks.hpp"
-#include "./include/Settings.hpp"
-#include "./include/stb_image.h"
+#include "include/Callbacks.hpp"
+#include "include/Camera.hpp"
+#include "include/Mesh.hpp"
 #include "include/Model.hpp"
+#include "include/Settings.hpp"
 #include "include/Shader.hpp"
+#include "include/stb_image.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <OpenGL/glu.h>
@@ -17,6 +17,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <sys/_types/_u_char.h>
 #include <sys/_types/_u_int.h>
 #include <sys/types.h>
@@ -24,6 +25,10 @@
 using namespace glm;
 using namespace std;
 using namespace Assimp;
+
+Camera camera(vec3(0.0f, 1.95f, 0.0f));
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 void initOpenGLProgram(GLFWwindow *window) {
   stbi_set_flip_vertically_on_load(true);
@@ -39,37 +44,14 @@ void initOpenGLProgram(GLFWwindow *window) {
 }
 
 void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
-
-void updateCameraMovement() {
-  // Calculate forward vector from yaw only (no pitch for vertical movement)
-  vec3 forward(sin(settings.cameraYaw), 0.0f, cos(settings.cameraYaw));
-
-  // Right vector (perpendicular to forward, in horizontal plane)
-  vec3 right(sin(settings.cameraYaw + M_PI / 2.0f), 0.0f,
-             cos(settings.cameraYaw + M_PI / 2.0f));
-
-  // Up vector
-  vec3 up(0.0f, 1.0f, 0.0f);
-
-  // W - forward
-  if (settings.keys[GLFW_KEY_W]) {
-    settings.cameraPos += forward * settings.cameraSpeed;
-  }
-  // S - backward
-  if (settings.keys[GLFW_KEY_S]) {
-    settings.cameraPos -= forward * settings.cameraSpeed;
-  }
-  // A - left
-  if (settings.keys[GLFW_KEY_A]) {
-    settings.cameraPos += right * settings.cameraSpeed;
-  }
-  // D - right
-  if (settings.keys[GLFW_KEY_D]) {
-    settings.cameraPos -= right * settings.cameraSpeed;
-  }
+  if (settings.keys[GLFW_KEY_W])
+    camera.ProcessKeyboard(FORWARD, deltaTime * 2.5f);
+  if (settings.keys[GLFW_KEY_S])
+    camera.ProcessKeyboard(BACKWARD, deltaTime * 2.5f);
+  if (settings.keys[GLFW_KEY_A])
+    camera.ProcessKeyboard(LEFT, deltaTime * 2.5f);
+  if (settings.keys[GLFW_KEY_D])
+    camera.ProcessKeyboard(RIGHT, deltaTime * 2.5f);
 }
 
 int main(void) {
@@ -111,8 +93,11 @@ int main(void) {
   initOpenGLProgram(window);
 
   while (!glfwWindowShouldClose(window)) {
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
     processInput(window);
-    updateCameraMovement();
     glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -123,7 +108,7 @@ int main(void) {
     static vector<Model> bottles = {};
     static bool bottlesLoaded = false;
     if (!bottlesLoaded) {
-      for (int i = 1; i <= 3; i++) {
+      for (int i = 1; i <= 5; i++) {
         string path = "models/bottles/bottle" + to_string(i) + ".obj";
         Model bottle(path.c_str());
         bottles.push_back(bottle);
@@ -133,13 +118,7 @@ int main(void) {
 
     static mat4 P = perspective(radians(50.0f), 1.0f, 0.1f, 50.0f);
 
-    mat4 V =
-        lookAt(settings.cameraPos,
-               settings.cameraPos +
-                   vec3(sin(settings.cameraYaw) * cos(settings.cameraPitch),
-                        sin(settings.cameraPitch),
-                        cos(settings.cameraYaw) * cos(settings.cameraPitch)),
-               vec3(0.0f, 1.0f, 0.0f));
+    mat4 V = camera.GetViewMatrix();
 
     defautlShader.use();
     defautlShader.setMat4("P", P);
